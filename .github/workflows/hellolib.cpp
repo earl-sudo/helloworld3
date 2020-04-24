f1a2e0d1db2dd86543d90c02a0bb33a7ca602a7c
earl-sudo <earl_johnson@verizon.net>
2020-04-23 17:51:04 -0700

    Removed un-required fiels and tried another way to do workflow.

--- .github/workflows/hellolib.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-void hello(const char* val) {
-   printf("Hello %s\n", val);
-}
--- .github/workflows/helloworld.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-void hello(const char* val);
-
-int main(int argc, char* argv[]) {
-     hello(argv[1]);
-     return 0;
-}
